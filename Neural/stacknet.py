b3b1767cc498a98114ad8b939f977e65d42df65c
/media/remote_home/va02 <jutanke@googlemail.com>
2018-01-07 00:24:14 +0100

    make the network train

--- Neural/stacknet.py
+++ Neural/stacknet.py
@@ -6,39 +6,39 @@ from keras.models import Sequential
 import numpy as np
 from keras.optimizers import SGD
 
-def get_model():
+def get_model(lr=0.01):
     vgg_model = VGG16(input_shape=(112, 112, 3), weights='imagenet',
                       include_top=False)
 
     train_upper_layers = True
     model = Sequential()
-    block1_conv1 = Conv2D(64, (3, 3), input_shape=(112,112,6), 
-                          padding='same', name='block1_conv1', 
+    block1_conv1 = Conv2D(64, (3, 3), input_shape=(112,112,6),
+                          padding='same', name='block1_conv1',
                           trainable=train_upper_layers)
     model.add(block1_conv1)
-    model.add(Conv2D(64, (3,3), padding='same', name='block1_conv2', 
+    model.add(Conv2D(64, (3,3), padding='same', name='block1_conv2',
                      trainable=train_upper_layers))
     model.add(MaxPooling2D(pool_size=(2,2), name='block1_pool'))
 
-    model.add(Conv2D(128, (3,3), padding='same', name='block2_conv1', 
+    model.add(Conv2D(128, (3,3), padding='same', name='block2_conv1',
                      trainable=train_upper_layers))
-    model.add(Conv2D(128, (3,3), padding='same', name='block2_conv2', 
+    model.add(Conv2D(128, (3,3), padding='same', name='block2_conv2',
                      trainable=train_upper_layers))
     model.add(MaxPooling2D(pool_size=(2,2), name='block2_pool'))
 
-    model.add(Conv2D(256, (3,3), padding='same', name='block3_conv1', 
+    model.add(Conv2D(256, (3,3), padding='same', name='block3_conv1',
                      trainable=train_upper_layers))
-    model.add(Conv2D(256, (3,3), padding='same', name='block3_conv2', 
+    model.add(Conv2D(256, (3,3), padding='same', name='block3_conv2',
                      trainable=train_upper_layers))
-    model.add(Conv2D(256, (3,3), padding='same', name='block3_conv3', 
+    model.add(Conv2D(256, (3,3), padding='same', name='block3_conv3',
                      trainable=train_upper_layers))
     model.add(MaxPooling2D(pool_size=(2,2), name='block3_pool'))
 
-    model.add(Conv2D(512, (3,3), padding='same', name='block4_conv1', 
+    model.add(Conv2D(512, (3,3), padding='same', name='block4_conv1',
                      trainable=train_upper_layers))
-    model.add(Conv2D(512, (3,3), padding='same', name='block4_conv2', 
+    model.add(Conv2D(512, (3,3), padding='same', name='block4_conv2',
                      trainable=train_upper_layers))
-    model.add(Conv2D(512, (3,3), padding='same', name='block4_conv3', 
+    model.add(Conv2D(512, (3,3), padding='same', name='block4_conv3',
                      trainable=train_upper_layers))
     model.add(MaxPooling2D(pool_size=(2,2), name='block4_pool'))
 
@@ -61,9 +61,9 @@ def get_model():
     for i in range(1, len(vgg_model.layers)-1):
         model.layers[i].set_weights(vgg_model.layers[i+1].get_weights())
 
-    sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+    sgd = SGD(lr=lr, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(optimizer=sgd, loss='categorical_crossentropy')
-    
+
     return model
 
-        
+
--- Neural/train_stacknet.py
+++ Neural/train_stacknet.py
@@ -3,17 +3,44 @@ from pprint import pprint
 Settings = json.load(open('../prototyping/settings.txt'))
 pprint(Settings)
 from CUHK03_Sampler import CUHK03_Sampler
-
+from keras.callbacks import ModelCheckpoint
+from keras.models import load_model
 from stacknet import get_model
+from os.path import join, isfile, isdir, exists, splitext
 
-model = get_model()
+
+root = Settings['data_root']
+filepath = join(root, 'stacknet_model.h5')
+checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+callbacks_list = [checkpoint]
+
+if isfile(filepath):
+    model = load_model(filepath)
+else:
+    model = get_model()
+
 model.summary()
 
+sampler = CUHK03_Sampler()
+
 def generate_training_data():
-    sampler = CUHK03_Sampler()
+    global sampler
     while True:
         X, Y = sampler.get_train_batch(100, 100)
         yield X, Y
 
-model.fit_generator(generate_training_data(), samples_per_epoch=10, 
-                    nb_epoch=10)
+def generate_validation_data():
+    global sampler
+    while True:
+        X, Y = sampler.get_test_batch(100, 100)
+        yield X, Y
+
+
+model.fit_generator(generate_training_data(),
+                    validation_data=generate_validation_data(),
+                    validation_steps=5,
+                    steps_per_epoch=100,
+                    epochs=1000,
+                    callbacks=callbacks_list)
+
+# load_model to load model
