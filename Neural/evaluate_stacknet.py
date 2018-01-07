8cb03022d99a3c7ff49a0794adb8facda8ac54d5
Kiwan <m.sabaa.kiwan@gmail.com>
2018-01-07 18:17:48 +0100

    evaluate

---
+++ Neural/evaluate_stacknet.py
@@ -0,0 +1,44 @@
+import json
+from pprint import pprint
+Settings = json.load(open('../prototyping/settings.txt'))
+pprint(Settings)
+from CUHK03_Sampler import CUHK03_Sampler
+from keras.callbacks import ModelCheckpoint, TerminateOnNaN
+from keras.models import load_model
+from keras.applications.vgg16 import preprocess_input
+from stacknet import get_model
+from os.path import join, isfile, isdir, exists, splitext
+import numpy as np
+import sys
+
+sys.path.insert(0,"../")
+
+from cabbage.data import ReId
+
+
+root = Settings['data_root']
+filepath = join(root, 'stacknet_model.h5')
+checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+callbacks_list = [checkpoint, TerminateOnNaN()]
+
+if isfile(filepath):
+    model = load_model(filepath)
+else:
+    raise Exception("Could not find model!")
+
+model.summary()
+
+sampler = ReId.DataSampler(root,112,112)
+X, Y = sampler.get_test_batch(1000, 4000)
+X = preprocess_input(X.astype('float64'))
+
+Y_ = model.predict(X)
+
+Y_clipped = (Y_[:,0] > 0.5) * 1
+Yclipped =  (Y[:,0] > 0.5) * 1
+
+accuracy = sum( Y_clipped * Yclipped) / len (Yclipped)
+
+
+print("accuracy :   \t", accuracy)
+
