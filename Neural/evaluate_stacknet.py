12a0eec97071f69e4b282f16a623874c5a7db6f6
/media/remote_home/va02 <jutanke@googlemail.com>
2018-01-07 18:30:16 +0100

    fix evaluation code for reid

--- Neural/evaluate_stacknet.py
+++ Neural/evaluate_stacknet.py
@@ -17,7 +17,7 @@ from cabbage.data import ReId
 
 
 root = Settings['data_root']
-filepath = join(root, 'stacknet_model.h5')
+filepath = join(root, 'stacknet_64x64_model.h5')
 checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
 callbacks_list = [checkpoint, TerminateOnNaN()]
 
@@ -28,7 +28,7 @@ else:
 
 model.summary()
 
-sampler = ReId.DataSampler(root,112,112)
+sampler = ReId.DataSampler(root,64,64)
 X, Y = sampler.get_test_batch(1000, 4000)
 X = preprocess_input(X.astype('float64'))
 
@@ -37,7 +37,7 @@ Y_ = model.predict(X)
 Y_clipped = (Y_[:,0] > 0.5) * 1
 Yclipped =  (Y[:,0] > 0.5) * 1
 
-accuracy = sum( Y_clipped * Yclipped) / len (Yclipped)
+accuracy = np.sum( (Y_clipped == Yclipped) * 1) / len (Yclipped)
 
 
 print("accuracy :   \t", accuracy)
