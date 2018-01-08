39b0fc957316e3b2a03fe2b3628afa87a925449e
Julian <jutanke@googlemail.com>
2018-01-08 22:59:10 +0000

    add MOT16 dataset reid

---
+++ Neural/evaluate_good_model.py
@@ -0,0 +1,47 @@
+import json
+from pprint import pprint
+Settings = json.load(open('../prototyping/settings.txt'))
+pprint(Settings)
+from CUHK03_Sampler import CUHK03_Sampler
+from keras.callbacks import ModelCheckpoint, TerminateOnNaN
+from keras.models import load_model
+from keras.applications.vgg16 import preprocess_input
+from stacknet import get_model
+from os.path import join, isfile, isdir, exists, splitext
+import numpy as np
+import sys
+
+sys.path.insert(0,"../")
+
+from cabbage.data import ReId
+from cabbage.data.MOT16Sampler import MOT16Sampler
+
+
+root = Settings['data_root']
+model_root = join(root, 'models')
+filepath = join(model_root, 'stacknet64x64_77acc.h5')
+checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+callbacks_list = [checkpoint, TerminateOnNaN()]
+
+if isfile(filepath):
+    model = load_model(filepath)
+else:
+    raise Exception("Could not find model!")
+
+model.summary()
+
+#sampler = ReId.DataSampler(root,64,64)
+sampler = MOT16Sampler(root, (64, 64))
+
+X, Y = sampler.get_named_batch('MOT16-02', 1000, 4000)
+X = preprocess_input(X.astype('float64'))
+
+Y_ = model.predict(X)
+
+Y_clipped = (Y_[:,0] > 0.5) * 1
+Yclipped =  (Y[:,0] > 0.5) * 1
+
+accuracy = np.sum( (Y_clipped == Yclipped) * 1) / len (Yclipped)
+
+
+print("accuracy :   \t", accuracy)
