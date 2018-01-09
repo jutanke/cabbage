33d4d1f821efe1d1ce61f5c2c3ca546e1824aec0
Julian <jutanke@googlemail.com>
2018-01-09 19:46:13 +0000

    add file handling for regression

--- cabbage/regression/Regression.py
+++ cabbage/regression/Regression.py
@@ -1,1 +1,28 @@
 import numpy as np
+from os import makedirs, listdir
+from os.path import join, isfile, isdir, exists, splitext
+
+
+class Regression:
+
+    def __init__(self, root, dmax):
+        """ Regression
+        """
+        self.root = join(root, 'regression' + dmax)
+        if not isdir(self.root):
+            makedirs(self.root)
+        self.dmax = dmax
+
+
+    def get_filename_for_theta(self, t):
+        """ generate the filename for theta_t
+        """
+        file_name = "w_" + str(t) + '.npy'
+        return join(self.root, file_name)
+
+
+    def run(self):
+        """ run the regression
+        """
+        #TODO implement
+        pass
