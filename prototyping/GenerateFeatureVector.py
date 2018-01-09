6f8e2a563aa7a834ddc401942990ac1b48b9d2e7
Kiwan <m.sabaa.kiwan@gmail.com>
2018-01-09 20:43:51 +0100

    regresin

--- prototyping/GenerateFeatureVector.py
+++ prototyping/GenerateFeatureVector.py
@@ -18,12 +18,16 @@ class pairwise_features:
         self.Settings = json.load(open('settings.txt'))
         self.root = self.Settings['data_root']
         self.dm = DeepMatching(self.Settings['deepmatch'], join(self.root, 'deep_matching'),delta_max=delta_max)
+        self.stacknet = StackNet64x64(self.root)
 
         
     def get_pairwise_vector(self,video_name,I1, I2, frame1,frame2,bb1,bb2,conf1,conf2):
+        i1 = get_element(I1, bb1, (64,64), force_uint=True)
+        i2 = get_element(I2, bb2, (64,64), force_uint=True)        
+        
         st_cost = st.calculate(bb1, bb2)
         dm_cost = self.dm.calculate_cost(video_name, frame1, bb1, frame2, bb2)
-        reid_cost = 1
+        reid_cost = self.stacknet.predict(i1, i2)[0][0]
         min_conf = min(conf1,conf2)
         
         return (st_cost , dm_cost , reid_cost , min_conf , \
