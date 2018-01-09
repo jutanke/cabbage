3370e4487e1a136850f36bb985bef97be73bf96b
/media/remote_home/va02 <jutanke@googlemail.com>
2018-01-09 17:28:20 +0100

    train stacknet with MOT16

--- Neural/j/train_64x64_stacknet.py
+++ Neural/j/train_64x64_stacknet.py
@@ -11,6 +11,9 @@ from stacknet import get_model
 from os.path import join, isfile, isdir, exists, splitext
 
 from cabbage.data import ReId
+from cabbage.data.MOT16Sampler import MOT16Sampler
+
+import numpy as np
 
 root = Settings['data_root']
 filepath = join(root, 'stacknet_64x64_model.h5')
@@ -25,18 +28,27 @@ else:
 model.summary()
 
 sampler = ReId.DataSampler(root, 64, 64)
+sampler_train = MOT16Sampler(root, (64, 64))
 
 def generate_training_data():
     global sampler
     while True:
-        X, Y = sampler.get_train_batch(100, 100)
+        X, Y = sampler_train.get_all_batch(20, 20)
+        X_, Y_ = sampler.get_train_batch(100, 100)
+        X = np.concatenate([X, X_])
+        Y = np.concatenate([Y, Y_])
         X = preprocess_input(X.astype('float64'))
         yield X, Y
 
 def generate_validation_data():
     global sampler
     while True:
-        X, Y = sampler.get_test_batch(100, 100)
+        X, Y = sampler_train.get_all_batch(20, 20)
+        X_, Y_ = sampler.get_test_batch(100, 100)
+
+        X = np.concatenate([X, X_])
+        Y = np.concatenate([Y, Y_])
+
         X = preprocess_input(X.astype('float64'))
         yield X, Y
 
