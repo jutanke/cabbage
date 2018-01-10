0ac0bdf13bd63a8d5d5822161575046e730c439c
/media/remote_home/va02 <jutanke@googlemail.com>
2018-01-10 23:42:56 +0100

    wip

--- prototyping/End_to_End.py
+++ prototyping/End_to_End.py
@@ -5,7 +5,7 @@ Settings = json.load(open('settings.txt'))
 pprint(Settings)
 import sys
 sys.path.append('../')
-from cabbage.regression.Regression import ReadOnlyRegression 
+from cabbage.regression.Regression import ReadOnlyRegression, get_W_mot16_02_dmax100
 from cabbage.MultiplePeopleTracking import GraphGenerator
 from cabbage.features.deepmatching import ReadOnlyDeepMatching
 from cabbage.features.ReId import StoredReId
@@ -18,8 +18,9 @@ video_name = 'MOT16-02'
 video = mot16.mot16_02_X
 dmax = 100
 
-regression = ReadOnlyRegression(root, 'MOT16-11', dmax)
-W = regression.get_weights()
+#regression = ReadOnlyRegression(root, 'MOT16-11', dmax)
+#W = regression.get_weights()
+W = get_W_mot16_02_dmax100()
 print(W.shape)
 
 
@@ -34,5 +35,5 @@ Dt = mot16.mot16_02_detections
 Hy = mot16.mot16_11_true_detections
 
 gg = GraphGenerator(root, video, Dt, dmax, W, video_name=video_name,
-                    DM_object=dm, reid_object=reid, 
+                    DM_object=dm, reid_object=reid,
                     is_memorized_reid=True)
