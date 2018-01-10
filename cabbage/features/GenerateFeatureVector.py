6f19928da683988bcb155ea98d28679805cbf28f
Julian <jutanke@googlemail.com>
2018-01-10 00:11:26 +0000

    fix oob bbs

--- cabbage/features/GenerateFeatureVector.py
+++ cabbage/features/GenerateFeatureVector.py
@@ -9,31 +9,31 @@ import json
 
 
 class pairwise_features:
-    """ generate pairwise features 
+    """ generate pairwise features
     """
     def __init__(self,root,delta_max,deep_matching_binary=None, dm_data_loc=None):
         """ ctor
         """
-        
+
         self.root = root
-        
+
         dm_only_eval = deep_matching_binary is None
         if dm_data_loc is None:
             dm_data_loc = join(root, 'deep_matching')
-            
+
         self.dm = DeepMatching(deep_matching_binary,dm_data_loc, delta_max, only_eval=dm_only_eval)
         self.stacknet = StackNet64x64(self.root)
 
-        
+
     def get_pairwise_vector(self,video_name,I1, I2, frame1,frame2,bb1,bb2,conf1,conf2):
         i1 = get_element(I1, bb1, (64,64), force_uint=True)
-        i2 = get_element(I2, bb2, (64,64), force_uint=True)        
-        
+        i2 = get_element(I2, bb2, (64,64), force_uint=True)
+
         st_cost = st.calculate(bb1, bb2)
         dm_cost = self.dm.calculate_cost(video_name, frame1, bb1, frame2, bb2)
         reid_cost = self.stacknet.predict(i1, i2)
         min_conf = min(conf1,conf2)
-        
+
         return (st_cost , dm_cost , reid_cost , min_conf , \
                 st_cost**2,st_cost * dm_cost,st_cost * reid_cost, st_cost * min_conf, \
                 dm_cost**2,dm_cost * reid_cost ,dm_cost * min_conf , \
