3f607332975340e343ed9a77f43966110aff0aa2
Julian <jutanke@googlemail.com>
2018-01-20 14:42:32 +0000

    wip: setup script

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 pip install git+https://github.com/justayak/cabbage.git
+pip install git+https://github.com/justayak/pak.git
+pip install git+https://github.com/justayak/cselect.git
+pip install git+https://github.com/justayak/pppr.git
 
-mkdir build 
+mkdir build
+(cd build && cmake .. && make -j8)
+
+mkdir deepmatching_bin
+(cd deepmatching_bin && wget http://lear.inrialpes.fr/src/deepmatching/code/deepmatching_1.2.2.zip && unzip deepmatching_1.2.2.zip)
