4242075c423c660d49e38bb82594573bc14cca49
Julian <jutanke@googlemail.com>
2018-01-20 14:46:33 +0000

    add install script

--- install.sh
+++ install.sh
@@ -6,5 +6,10 @@ pip install git+https://github.com/justayak/pppr.git
 mkdir build
 (cd build && cmake .. && make -j8)
 
-mkdir deepmatching_bin
-(cd deepmatching_bin && wget http://lear.inrialpes.fr/src/deepmatching/code/deepmatching_1.2.2.zip && unzip deepmatching_1.2.2.zip)
+if [ ! -d "deepmatching_bin" ]; then
+    mkdir deepmatching_bin
+    (cd deepmatching_bin && wget http://lear.inrialpes.fr/src/deepmatching/code/deepmatching_1.2.2.zip && unzip deepmatching_1.2.2.zip)
+fi
+
+
+python build_settings.py
