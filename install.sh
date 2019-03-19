d8ac08f72e70964aee5cbc4d1d3261d7156abf20
stasysp <stasysp.96@gmail.com>
2019-03-19 21:13:02 +0300

    Revert "Try to change install.sh"

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
-pip3 install tensorflow scikit-learn keras scikit-image
-
-pip3 install git+https://github.com/justayak/cabbage.git
-pip3 install git+https://github.com/justayak/pak.git
-pip3 install git+https://github.com/justayak/cselect.git
-pip3 install git+https://github.com/justayak/pppr.git
+pip install git+https://github.com/justayak/cabbage.git
+pip install git+https://github.com/justayak/pak.git
+pip install git+https://github.com/justayak/cselect.git
+pip install git+https://github.com/justayak/pppr.git
 
 mkdir build
 (cd build && cmake .. && make -j8)
@@ -12,5 +10,6 @@ if [ ! -d "deepmatching_bin" ]; then
     mkdir deepmatching_bin
     (cd deepmatching_bin && wget http://lear.inrialpes.fr/src/deepmatching/code/deepmatching_1.2.2.zip && unzip deepmatching_1.2.2.zip)
 fi
+
 
 python build_settings.py
